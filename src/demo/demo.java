9c7fd821219552e90d4b6b8ccd3fe5d6e5816746
翔哥 <894549548@qq.com>
2018-11-08 18:07:16 +0800

    d123

--- src/demo/demo.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("hello word");
-        Dog dog = new Dog("旺财");
-        System.out.println(dog);
-    }
-}
